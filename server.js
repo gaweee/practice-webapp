c8467f7accec525a3062548ae633603b1589e79d
Gary Teo <gary@wits.sg>
2016-12-25 11:07:27 +0800

    initial commit

---
+++ server.js
@@ -0,0 +1,34 @@
+var express = require("express");
+var app = express();
+
+
+var middleware = {
+	requireAuth: function(req, res, next) {
+		console.log('private route hit');
+		next();
+	},
+
+	logger: function(req, res, next) {
+		console.log('Request ' + new Date().toString() + ' ' + req.method + ' '  + req.originalUrl);
+		next();
+	}
+};
+
+app.use(middleware.logger);
+
+app.get('/about', middleware.requireAuth, function(req, res) {
+	res.send('About Us!');
+});
+
+// Setup Asset Folders
+app.use(express.static(__dirname + '/public'));
+
+
+// Setup Catch-All for 404
+app.get('*', function(req, res){
+  res.sendStatus(404);
+});
+
+app.listen(3000, function() {
+	console.log("Application started");
+});
