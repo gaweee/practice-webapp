4a3e795599ef945e7ab78f40063dff869aa3d7a8
Gary Teo <gary@wits.sg>
2016-12-25 22:20:40 +0800

    added Todos all API

--- server.js
+++ server.js
@@ -2,6 +2,19 @@ var express = require("express");
 var app = express();
 const PORT = process.env.PORT || 3000;
 
+var todos = [
+	{
+		id: 1,
+		title: 'Meet mom for lunch',
+		completed: false
+	},
+	{
+		id: 2,
+		title: 'Cook for wife',
+		completed: true
+	}
+];
+
 
 var middleware = {
 	requireAuth: function(req, res, next) {
@@ -19,6 +32,10 @@ app.use(middleware.logger);
 
 app.get('/about', middleware.requireAuth, function(req, res) {
 	res.send('About Us!');
+});
+
+app.get('/todos', function(req, res) {
+	res.json(todos);
 });
 
 // Setup Asset Folders
